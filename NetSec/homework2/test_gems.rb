c860ef521c522d36e51cd44beae3810af5054513
Zvezdi Dimitrova <zvezdi.dim@gmail.com>
2016-11-10 19:00:11 +0200

    Test dev

--- NetSec/homework2/test_gems.rb
+++ NetSec/homework2/test_gems.rb
@@ -1,17 +1,25 @@
-require 'rubygems'
 require 'ffi/pcap'
 
-pcap =
-  FFI::PCap::Live.new(:dev => 'lo0',
-                      :timeout => 1,
-                      :promisc => true,
-                      :handler => FFI::PCap::Handler)
+dev = FFI::PCap.device_names[0]  # or the active interface you want to get packets from
+                                  # need to figure out how to select it dinamically
 
-pcap.setfilter("icmp")
+pcap = FFI::PCap::Live.new(:dev => dev,
+                           :timeout => 1,
+                           :promisc => true,
+                           :handler => FFI::PCap::Handler)   # for this time, listening is active, as show with stats
 
-pcap.loop() do |this,pkt|
-  puts "#{pkt.time}:"
+pcap.stats
+pcap.stats.ps_recv
 
+pcap.datalink.describe
+
+pakt = []
+
+pcap.loop(:count => 1) do |this,pkt|   # :count => -1 for infinite loop, break with .breakloop method
+  pakt << pkt
+  puts "#{pkt.time} :: #{pkt.len}"
   pkt.body.each_byte {|x| print "%0.2x " % x }
   putc "\n"
 end
+
+puts pakt
